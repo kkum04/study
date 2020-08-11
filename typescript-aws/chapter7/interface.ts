86974c5e45a040cf34729ee6c20270f553e770d3
KenPark <kkum04@gmail.com>
2020-08-12 00:43:36 +0900

    typescript interface와 class 초기 스터디

---
+++ typescript-aws/chapter7/interface.ts
@@ -0,0 +1,42 @@
+interface Size {
+    width: number;
+    height: number;
+}
+
+interface Label {
+    title: string;
+    size: Size
+}
+
+function labelPrint(label: Label): void {
+    console.log(label);
+}
+
+let myLabel = <Label> {
+    title: '타입스크립트 도서', size: { width: 20, height: 29 }
+};
+
+labelPrint(myLabel);
+
+interface Config {
+    name: string;
+    path: string;
+    version?: string;
+}
+
+interface App {
+    fullPath: string;
+    version?: string;
+}
+
+function applicationInit(config: Config): App {
+    let app = {fullPath: config.path + config.name } as App
+    if (config.version) {
+        app.version = config.version
+    }
+
+    return app
+}
+
+console.log(applicationInit({path: '/home/', name: 'user'} as Config))
+console.log(applicationInit({path: '/home', name: 'user', version: '0.1.1'} as Config))
