1fca4a5e218bbd98b55bf4e6aa0560ffa695cec3
KenPark <kkum04@gmail.com>
2020-08-20 18:57:55 +0900

    typescript class

--- typescript-aws/chapter7/class.ts
+++ typescript-aws/chapter7/class.ts
@@ -8,9 +8,32 @@ class Animal {
     }
 
     info(): string {
-        return `${this.name} has ${this.legs} legs.`
+        return `${this.name}는 다리가  ${this.legs}개다.`
+    }
+
+    move(meters: number): string {
+        return this.name + "가 " + meters + " 미터를 움직였다.";
     }
 }
 
-let dog = new Animal("Happy")
+class Fish extends Animal {
+    constructor(name: string, legs: number = 0) {
+        super(name, legs);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name: string, legs: number = 4) {
+        super(name);
+    }
+
+    bark(sound: string) {
+        return sound + " " + sound
+    }
+}
+
+let dog: Dog = new Dog("멍멍이", 4);
 console.log(dog.info());
+console.log(dog.bark('멍!'));
+let fish: Fish = new Fish("니모");
+console.log(fish.move(10));
