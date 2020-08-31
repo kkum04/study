e6ec7f4b630f260718e501dd414c17d7a3788596
KenPark <kkum04@gmail.com>
2020-08-31 19:03:37 +0900

    method compoent 추가

---
+++ typescript-aws/chapter10/src/components/Method.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    COUNT : {{ currentCount }}
+    <input type="button" @click="add()" value="더하기" />
+    <input type="button" @click="currentCount =0" value="리셋" />
+  </div>
+
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from "vue-class-component";
+
+  @Component
+  export default class App extends Vue {
+    private count: number = 0;
+
+    private add() {
+      this.count++;
+    }
+
+    private get currentCount() {
+      return this.count;
+    }
+
+    private set currentCount(value: number) {
+      this.count = value;
+    }
+  }
+
+
+
+</script>
--- typescript-aws/chapter10/src/views/Home.vue
+++ typescript-aws/chapter10/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <method></method>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Method from "@/components/Method.vue";
 
 @Component({
   components: {
     HelloWorld,
+    Method
   },
 })
 export default class Home extends Vue {}
