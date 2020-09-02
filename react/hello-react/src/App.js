210797390117a99566332d52831df2ea49f251d8
KenPark <kkum04@gmail.com>
2020-09-03 01:10:57 +0900

    react props

--- react/hello-react/src/App.js
+++ react/hello-react/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MyCompoent from './MyComponent'
 
 const App = () => {
-  return <MyCompoent />
+  return <MyCompoent>리액트</MyCompoent>
 }
 
 export default App;
