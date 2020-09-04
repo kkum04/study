4476ca071f047e70b9905783f98cbfa465b9fff4
KenPark <kkum04@gmail.com>
2020-09-04 23:18:06 +0900

    react ref ˆˆ

--- react/hello-react/src/App.js
+++ react/hello-react/src/App.js
@@ -3,7 +3,14 @@ import ScrollBox from './ScrollBox'
 
 class App extends Component {
   render() {
-    return <ScrollBox />;
+    return (
+      <>
+        <ScrollBox ref={ref => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </>
+    )
   }
 }
 
