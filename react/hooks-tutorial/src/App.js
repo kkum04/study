83b118fda0dc53ee9aadde338b0e7c05f9bada30
KenPark <kkum04@gmail.com>
2020-09-08 13:54:23 +0900

    use Effect

--- react/hooks-tutorial/src/App.js
+++ react/hooks-tutorial/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import Info from "./Info";
-
 function App() {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <Info />
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기': '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
   );
 }
 
