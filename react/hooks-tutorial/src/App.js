929bfaac0c739d18d796a71cfe6b06289bd23163
KenPark <kkum04@gmail.com>
2020-09-08 14:25:57 +0900

    useMemo

--- react/hooks-tutorial/src/App.js
+++ react/hooks-tutorial/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import Info from "./Info";
 import Counter from "./Counter";
+import Average from "./Average";
 function App() {
   const [visible, setVisible] = useState(false);
 
   return (
     <div>
-      <Info />
+      <Average />
     </div>
   );
 }
