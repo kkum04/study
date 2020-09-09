0e0d79236b43c9043d34c0c06cd2cd11b01c7914
KenPark <kkum04@gmail.com>
2020-09-09 22:57:52 +0900

    CSS Module 사용법

--- react/styling-react/src/App.js
+++ react/styling-react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import SassComponent from "./SassComponent";
+//import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <CSSModule />
     </div>
   );
 }
