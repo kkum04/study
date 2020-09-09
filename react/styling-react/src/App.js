06aab18b985834328277a8ab3c97a90d8326e8c1
KenPark <kkum04@gmail.com>
2020-09-10 00:32:55 +0900

    styled component

--- react/styling-react/src/App.js
+++ react/styling-react/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import SassComponent from "./SassComponent";
-import CSSModule from "./CSSModule";
+//import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div>
-      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
