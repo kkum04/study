cbe14946cf98715977872c05c288ea893fd5cb24
KenPark <kkum04@gmail.com>
2020-09-10 15:49:07 +0900

    화면 구성 완료

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
-
   );
 }
 
