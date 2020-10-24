988d662f8b29aee48832be0dce0909088bf1398a
KenPark <kkum04@gmail.com>
2020-10-25 08:16:17 +0900

    tab

--- flutter/flutter_app/lib/main.dart
+++ flutter/flutter_app/lib/main.dart
@@ -21,30 +21,54 @@ class MyApp extends StatelessWidget {
 // 여기까지는 공통코드
 
 // 여기서부터 수정
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final items = List.generate(100, (i) => i).toList();
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('제목')),
-        body: PageView(
-          children: <Widget>[
-            Container(
-              color: Colors.red,
+    // TODO: implement build
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Tab'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.tag_faces)),
+                Tab(text: '메뉴2'),
+                Tab(icon: Icon(Icons.info), text: '메뉴3')
+              ],
             ),
-            Container(
-              color: Colors.green,
-            ),
-            Container(
-              color: Colors.blue
-            )
-          ],
-        ));
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(color: Colors.yellow),
+              Container(color: Colors.orange),
+              Container(color: Colors.red)
+            ],
+          ),
+        )
+    );
   }
 }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   final items = List.generate(100, (i) => i).toList();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(title: Text('제목')),
+//         body: PageView(
+//           children: <Widget>[
+//             Container(
+//               color: Colors.red,
+//             ),
+//             Container(
+//               color: Colors.green,
+//             ),
+//             Container(
+//               color: Colors.blue
+//             )
+//           ],
+//         ));
+//   }
+// }
