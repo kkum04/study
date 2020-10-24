52e1c8fc689b925d0e32ec989d49b63c93b0adf6
KenPark <kkum04@gmail.com>
2020-10-25 08:06:16 +0900

    pageview

--- flutter/flutter_app/lib/main.dart
+++ flutter/flutter_app/lib/main.dart
@@ -33,30 +33,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('제목')),
-        body: GridView.count(
-            crossAxisCount: 2,
+        body: PageView(
           children: <Widget>[
             Container(
               color: Colors.red,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
             ),
             Container(
               color: Colors.green,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
             ),
             Container(
-              color: Colors.blue,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-            ),
+              color: Colors.blue
+            )
           ],
         ));
   }
