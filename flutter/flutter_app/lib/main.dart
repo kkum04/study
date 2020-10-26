ea00c4f9cc18ab014cf474fbcf9a57abac7af817
KenPark <kkum04@gmail.com>
2020-10-26 17:34:37 +0900

    radio

--- flutter/flutter_app/lib/main.dart
+++ flutter/flutter_app/lib/main.dart
@@ -21,53 +21,79 @@ class MyApp extends StatelessWidget {
 // 여기까지는 공통코드
 
 // 여기서부터 수정
+enum Gender { MAN, WOMEN }
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final items = List.generate(100, (i) => i).toList();
-  var isChecked = false;
+  Gender _gender = Gender.MAN;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('제목')
-        ),
-        body: ListView(
-          children: [
-            TextField(),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '여기에 입력하세요.'
+      appBar: AppBar(
+        title: Text('Radio / RadioListTile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ListTile(
+                title: Text('남자'),
+                leading: Radio(
+                  value: Gender.MAN,
+                  groupValue: _gender,
+                  onChanged: (value) {
+                    setState(() {
+                      _gender = value;
+                    });
+                  },
+                ),
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '여기에 입력하세요.'
+              ListTile(
+                title: Text('여자'),
+                leading: Radio(
+                  value: Gender.WOMEN,
+                  groupValue: _gender,
+                  onChanged: (value) {
+                    setState(() {
+                      _gender = value;
+                    });
+                  },
+                ),
               ),
-            ),
-            Checkbox(
-              value: isChecked,
-              onChanged: (value) {
-                setState(() {
-                  isChecked = value;
-                });
-              }
-            ),
-            Switch(
-                value: isChecked,
+              SizedBox(
+                height: 40,
+              ),
+              RadioListTile(
+                title: Text('남자'),
+                value: Gender.MAN,
+                groupValue: _gender,
                 onChanged: (value) {
                   setState(() {
-                    isChecked = value;
+                    _gender = value;
                   });
-                }
-            )
-          ],
-        )
+                },
+              ),
+              RadioListTile(
+                title: Text('여자'),
+                value: Gender.WOMEN,
+                groupValue: _gender,
+                onChanged: (value) {
+                  setState(() {
+                    _gender = value;
+                  });
+                },
+              )
+            ],
+          )
+        ),
+      )
     );
   }
 }
