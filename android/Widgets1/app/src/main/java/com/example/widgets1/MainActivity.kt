af48c3cea3c6a33355b4e5f1afdb22bef1f1bdc2
KenPark <kkum04@gmail.com>
2020-11-21 12:18:41 +0900

    입력창, 이미지 버튼

---
+++ android/Widgets1/app/src/main/java/com/example/widgets1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.widgets1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("EditText", "현재 입력된 값=${s.toString()}")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s.toString().length >= 8) {
+                    Log.d("EditText", "8자 이상일 때만 출력=${s.toString()}")
+                }
+            }
+        })
+    }
+}
