a606316775aae9a99672ff2490e53eb761d05a5b
KenPark <kkum04@gmail.com>
2020-11-23 17:02:33 +0900

    서비스

---
+++ android/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.servicetest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun serviceStart(view: View) {
+        val intent = Intent(this, MyService::class.java)
+        intent.action = MyService.ACTION_START
+        startService(intent)
+    }
+
+    fun serviceStop(view: View) {
+        val intent = Intent(this, MyService::class.java)
+        stopService(intent)
+    }
+}
