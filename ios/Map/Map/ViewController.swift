6194052bd0b1fa28e50bc2d8f0ee24248ae9e0e6
KenPark <kkum04@gmail.com>
2020-12-31 18:01:34 +0900

    ios page control

---
+++ ios/Map/Map/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by KenPark on 2020/12/31.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var lblLocationInfo1: UILabel!
+    @IBOutlet var lblLocationInfo2: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lblLocationInfo1.text = ""
+        lblLocationInfo2.text = ""
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        myMap.showsUserLocation = true
+    }
+    
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        
+        myMap.setRegion(pRegion, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last
+        goLocation(
+            latitudeValue: (pLocation?.coordinate.latitude)!,
+            longitudeValue: (pLocation?.coordinate.longitude)!,
+            delta: 0.01
+        )
+
+        CLGeocoder().reverseGeocodeLocation(pLocation!) { (placemarks, error) -> Void in
+            let pm = placemarks!.first
+            let country = pm!.country
+            var address: String = country!
+            if pm!.locality != nil {
+                address += " "
+                address += pm!.locality!
+            }
+            if pm!.thoroughfare != nil {
+                address += " "
+                address += pm!.thoroughfare!
+            }
+
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address
+        }
+
+        locationManager.stopUpdatingLocation()
+    }
+
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+    }
+}
+
