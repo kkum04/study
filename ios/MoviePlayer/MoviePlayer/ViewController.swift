cefe67a80f3b9e28360556b5314301b55004afbb
KenPark <kkum04@gmail.com>
2021-01-03 18:36:37 +0900

    ios 비디오

---
+++ ios/MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by KenPark on 2021/01/03.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath: String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: Any) {
+    }
+}
+
