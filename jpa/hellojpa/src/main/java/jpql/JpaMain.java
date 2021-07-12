4be10332e364541d537cc10811bacb28ec00aaa9
KenPark <kkum04@gmail.com>
2021-07-12 23:58:05 +0900

    jpa 기본 문법과 쿼리 API

---
+++ jpa/hellojpa/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,36 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+      Member member = new Member();
+      member.setUsername("member1");
+      member.setAge(10);
+      em.persist(member);
+
+      System.out.println("============================");
+      TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+      Member member1 = query.getSingleResult();
+
+      System.out.println(member1);
+      System.out.println("============================");
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+    emf.close();
+  }
+}
