d02a4d8627ec8e6f34e63250f19181d016577113
kkum04 <kkum04@gmail.com>
2021-08-09 11:23:47 +0900

    리니어 레이아웃 스터디

---
+++ android/SimpleLinearLayout/app/src/main/java/org/techtown/push/simplelinearlayout/LayoutCodeActivity.kt
@@ -0,0 +1,27 @@
+package org.techtown.push.simplelinearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+
+class LayoutCodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val mainLayout = LinearLayout(this)
+        mainLayout.orientation = LinearLayout.VERTICAL
+
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        val button1 = Button(this)
+        button1.text = "Button1"
+        button1.layoutParams = params
+        mainLayout.addView(button1)
+
+        setContentView(mainLayout)
+    }
+}
