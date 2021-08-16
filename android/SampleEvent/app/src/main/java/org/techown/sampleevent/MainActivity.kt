88651779b72d50ae934829703bb04b6858b9ce55
KenPark <kkum04@gmail.com>
2021-08-16 11:33:39 +0900

    키 이벤트 처리

--- android/SampleEvent/app/src/main/java/org/techown/sampleevent/MainActivity.kt
+++ android/SampleEvent/app/src/main/java/org/techown/sampleevent/MainActivity.kt
@@ -2,12 +2,17 @@ package org.techown.sampleevent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
   lateinit var textView: TextView
+
+  lateinit var detector: GestureDetector
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -32,6 +37,54 @@ class MainActivity : AppCompatActivity() {
 
       true
     }
+
+    detector = GestureDetector(this, object : GestureDetector.OnGestureListener {
+      override fun onDown(motionEvent: MotionEvent): Boolean {
+        println("onDown() 호출됨.")
+        return true
+      }
+
+      override fun onShowPress(p0: MotionEvent?) {
+        println("onShowPress() 호출됨")
+      }
+
+      override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        println("onSingleTapUp() 호출됨")
+        return true
+      }
+
+      override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        println("onScroll() 호출됨")
+
+        return true
+      }
+
+      override fun onLongPress(p0: MotionEvent?) {
+        println("onLongPress() 호출됨")
+      }
+
+      override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        println("onFling() 호출됨")
+
+        return true
+      }
+    })
+
+    val view2 = findViewById<View>(R.id.view2)
+    view2.setOnTouchListener { p0, p1 ->
+      detector.onTouchEvent(p1)
+      true
+    }
+  }
+
+  override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      Toast.makeText(this, "시스템 [BACK] 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show()
+
+      return true
+    }
+
+    return false
   }
 
   fun println(data: String) {
