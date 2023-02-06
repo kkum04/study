288c62573289e28113a6149eba402470f0aa6a83
KenPark <kkum04@gmail.com>
2023-02-06 22:05:37 +0900

    Containerless 개발 준비

---
+++ Helloboot/src/main/java/kenspring/helloboot/helloboot/HellobootApplication.java
@@ -0,0 +1,9 @@
+package kenspring.helloboot.helloboot;
+
+public class HellobootApplication {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Containerless Standalone Application");
+  }
+
+}
