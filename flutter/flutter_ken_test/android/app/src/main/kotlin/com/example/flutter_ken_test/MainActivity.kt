d57b6d8ca84d66ce077b323f06c8d57650202d03
taehwanpark <kkum04@gmail.com>
2023-02-14 16:10:27 +0900

    flutter 3장 완료

---
+++ flutter/flutter_ken_test/android/app/src/main/kotlin/com/example/flutter_ken_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ken_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/flutter_ken_test/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _text = 'Hello';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello World"),
+      ),
+      body: Text(
+        _text,
+        style: const TextStyle(fontSize: 40)
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _text = 'World'),
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
