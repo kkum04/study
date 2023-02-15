c03a87a4dd7c6b553d44dff2286f7111d48e4374
taehwanpark <kkum04@gmail.com>
2023-02-15 17:40:49 +0900

    dialog

--- flutter/flutter_ken_test/lib/main.dart
+++ flutter/flutter_ken_test/lib/main.dart
@@ -17,69 +17,90 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-enum Gender { MAN, WOMEN }
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _valueList = ['첫 번째', '두 번째', '세 번째'];
-  var _selectedValue = '첫 번째';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Radio /RadioListTile"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: DropdownButton(
-            value: _selectedValue,
-            items: _valueList.map((e) {
-              return DropdownMenuItem(
-                value: e,
-                child: Text(e),
-              );
-            }).toList(),
-            onChanged: (value) => {setState(() { _selectedValue = value ?? '첫 번째'; })},
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// class MyHomePage extends StatelessWidget {
+// class MyHomePage extends StatefulWidget {
 //   const MyHomePage({super.key});
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// enum Gender { MAN, WOMEN }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   final _valueList = ['첫 번째', '두 번째', '세 번째'];
+//   var _selectedValue = '첫 번째';
 //
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//         appBar: AppBar(
-//           title: const Text("Hello World"),
-//         ),
-//         body: Padding(
-//           padding: const EdgeInsets.all(20.0),
-//           child: Column(
-//             children: const [
-//               TextField(),
-//               TextField(
-//                 decoration: InputDecoration(labelText: '여기에 입력하세요.'),
-//               ),
-//               TextField(
-//                 decoration: InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     labelText: '여기에 입력하세요'
-//                 ),
-//               )
-//             ],
+//       appBar: AppBar(
+//         title: const Text("Radio /RadioListTile"),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Center(
+//           child: DropdownButton(
+//             value: _selectedValue,
+//             items: _valueList.map((e) {
+//               return DropdownMenuItem(
+//                 value: e,
+//                 child: Text(e),
+//               );
+//             }).toList(),
+//             onChanged: (value) => {setState(() { _selectedValue = value ?? '첫 번째'; })},
 //           ),
-//         ));
+//         ),
+//       ),
+//     );
 //   }
 // }
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello World"),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: TextButton(
+                child: const Text('버튼'),
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('제목'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: const [
+                                Text('Alert Dialog입니다.'),
+                                Text('OK를 눌러 닫습니다.')
+                              ],
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('OK'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Cancel'),
+                            )
+                          ],
+                        );
+                      });
+                },
+              ),
+            )));
+  }
+}
