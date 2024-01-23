1867aaf637b0a950b5deebb255ba642fecac9243
KenPark <kkum04@gmail.com>
2024-01-23 21:54:35 +0900

    linking and navigatingˆ

--- nextjs/my-nextjs-first-app/app/dashboard/page.tsx
+++ nextjs/my-nextjs-first-app/app/dashboard/page.tsx
@@ -1,3 +1,24 @@
+import React from "react";
+
 export default function Page() {
-  return <h1>Hello, Dashboard Page!</h1>
+  return <div>
+    <h1>Hello, Dashboard Page!</h1>
+    <h1>Scroll1</h1>
+    <h1>Scroll2</h1>
+    <h1>Scroll3</h1>
+    <h1>Scroll4</h1>
+    <h1>Scroll5</h1>
+    <h1>Scroll6</h1>
+    <h1>Scroll7</h1>
+    <h1>Scroll8</h1>
+    <h1>Scroll9</h1>
+    <h1>Scroll10</h1>
+    <h1>Scroll11</h1>
+    <h1>Scroll12</h1>
+    <h1>Scroll13</h1>
+    <h1>Scroll14</h1>
+    <h1 id="scrollTarget">Scroll15</h1>
+    <h1>Scroll16</h1>
+  </div>
+
 }
--- nextjs/my-nextjs-first-app/app/page.tsx
+++ nextjs/my-nextjs-first-app/app/page.tsx
@@ -1,9 +1,30 @@
 import {Metadata} from "next";
+import React from "react";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: 'Next.js'
 }
 export default function page() {
-  return <h1>Hello, Home page!</h1>
+  return <div>
+    <h1>Hello, Home page!</h1>
+    <br/>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <Link href="/dashboard#scrollTarget">Goto Dashboard with scroll</Link>
+    <br/>
+    <Link href="/dashboard" scroll={false}>Goto Dashboard without scroll</Link>
+
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+    <h1>Temp</h1>
+  </div>
 }
 
