af6ccbd410f6fbc08d92e4b3c760aaf07ccce71d
KenPark <kkum04@gmail.com>
2024-01-24 11:07:05 +0900

    error 페이지 추가

--- nextjs/my-nextjs-first-app/app/dashboard/page.tsx
+++ nextjs/my-nextjs-first-app/app/dashboard/page.tsx
@@ -1,10 +1,5 @@
-"use client"
 import React from "react";
-import {useRouter} from "next/navigation";
-
 export default function Page() {
-  const router = useRouter();
-
   return <div>
     <h1>Hello, Dashboard Page!</h1>
     <h1>Scroll1</h1>
