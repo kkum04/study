263a7cbbdd967380c5155b5cc2a24b653902a447
KenPark <kkum04@gmail.com>
2024-07-22 21:55:31 +0900

    array sortˆ

---
+++ moden-javascript/chapter_27/example.js
@@ -0,0 +1,16 @@
+const todos = [
+  {id: 4, content: 'javascript'},
+  {id: 1, content: 'HTML'},
+  {id: 2, content: 'CSS'},
+];
+
+function compare(key) {
+    return (a, b) => (a[key] > b[key] ? 1 : (a[key] < b[key] ? -1 : 0));
+}
+
+todos.sort(compare("id"));
+console.log(todos);
+
+todos.sort(compare("content"));
+console.dir(todos);
+
